e88fb9ff437bbef095925063b04b11fe45fabc79
AramPetrosyan0 <aspetrosyan07@gmail.com>
2024-05-14 17:51:23 +0400

    change base url

--- src/axios.js
+++ src/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const instance = axios.create({
-  // baseURL: "https://ntl-api.vercel.app/",
-  baseURL: "http://localhost:4000/",
+  baseURL: "ntl-api.vercel.app/",
+  // baseURL: "http://localhost:4000/",
 });
 
 instance.interceptors.request.use((config) => {
